7388d3202b322d28ab30aa384e12bc2002e2d20d
Ostretsova Anna <ostretsova.ann@gmail.com>
2021-02-07 17:46:56 +0300

    компоненты для всех страниц, установка react-router-dom, описание маршрутов приложения

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
-import MainPage from '../main-page/main-page';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import MainPage from '../main-page/main-page';
+import NotFoundPage from "../not-found-page/not-found-page";
+import Login from "../login/login";
+import Favorites from "../favorites/favorites";
+import Property from "../property/property";
 
 const App = (props) => {
 
   return (
-    <MainPage
-      names={props.names}
-      placesCount={props.placesCount}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage
+            names={props.names}
+            placesCount={props.placesCount}
+          />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/offer/:id?">
+          <Property />
+        </Route>
+        <Route>
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
