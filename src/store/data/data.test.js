61a515b97704757d704d6dcd9f1b9ba55ba94887
Ostretsova Anna <ostretsova.ann@gmail.com>
2021-03-30 12:53:27 +0300

    автотест на user.js

--- src/store/data/data.test.js
+++ src/store/data/data.test.js
@@ -9,6 +9,7 @@ import {
   changeIsFavoriteProperty
 } from '../action';
 import {data} from './data';
+import {changeFavoritePlaces, updatePlaces, updateNearPlaces} from '../../common/utils';
 
 describe(`Reducers work correctly`, () => {
   it(`Reducer with no additional parameters should return initial state`, () => {
@@ -366,20 +367,128 @@ describe(`Reducers work correctly`, () => {
       }], isNearbyLoaded: true});
   });
 
-  // it(`Reducer should update favorite place`, () => {
-  //   const state = {
-  //     favoritesPlaces: [{id: 1}],
-  //     places: [{id: 1}],
-  //     nearPlaces: [{id: 1}],
-  //   };
-  //
-  //   expect(data(state, updateFavoritePlace()))
-  //     .toEqual({
-  //       favoritesPlaces: [{id: 1}],
-  //       places: [{id: 1}],
-  //       nearPlaces: [{id: 1}],
-  //     });
-  // });
+  it(`Reducer should update favorite place`, () => {
+    const places = [{
+      "bedrooms": 3,
+      "city": {
+        "location": {
+          "latitude": 52.370216,
+          "longitude": 4.895168,
+          "zoom": 10
+        },
+        "name": `Amsterdam`
+      },
+      "description": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+      "goods": [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
+      "host": {
+        "avatar_url": `img/1.png`,
+        "id": 3,
+        "is_pro": true,
+        "name": `Angelina`
+      },
+      "id": 1,
+      "images": [`img/1.png`, `img/2.png`],
+      "isFavorite": false,
+      "is_premium": false,
+      "location": {
+        "latitude": 52.35514938496378,
+        "longitude": 4.673877537499948,
+        "zoom": 8
+      },
+      "max_adults": 4,
+      "preview_image": `img/1.png`,
+      "price": 120,
+      "rating": 4.8,
+      "title": `Beautiful & luxurious studio at great location`,
+      "type": `apartment`
+    },
+    {
+      "bedrooms": 1,
+      "city": {
+        "location": {
+          "latitude": 52.370216,
+          "longitude": 4.895168,
+          "zoom": 10
+        },
+        "name": `Amsterdam`
+      },
+      "description": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+      "goods": [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
+      "host": {
+        "avatar_url": `img/1.png`,
+        "id": 3,
+        "is_pro": true,
+        "name": `Angelina`
+      },
+      "id": 1,
+      "images": [`img/1.png`, `img/2.png`],
+      "isFavorite": true,
+      "is_premium": false,
+      "location": {
+        "latitude": 52.35514938496378,
+        "longitude": 4.673877537499948,
+        "zoom": 8
+      },
+      "max_adults": 4,
+      "preview_image": `img/1.png`,
+      "price": 120,
+      "rating": 4.8,
+      "title": `Beautiful & luxurious studio at great location`,
+      "type": `apartment`
+    }];
+
+    const place = {
+      "bedrooms": 3,
+      "city": {
+        "location": {
+          "latitude": 52.370216,
+          "longitude": 4.895168,
+          "zoom": 10
+        },
+        "name": `Amsterdam`
+      },
+      "description": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+      "goods": [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
+      "host": {
+        "avatar_url": `img/1.png`,
+        "id": 3,
+        "is_pro": true,
+        "name": `Angelina`
+      },
+      "id": 1,
+      "images": [`img/1.png`, `img/2.png`],
+      "isFavorite": false,
+      "is_premium": false,
+      "location": {
+        "latitude": 52.35514938496378,
+        "longitude": 4.673877537499948,
+        "zoom": 8
+      },
+      "max_adults": 4,
+      "preview_image": `img/1.png`,
+      "price": 120,
+      "rating": 4.8,
+      "title": `Beautiful & luxurious studio at great location`,
+      "type": `apartment`
+    };
+
+    const updatedFavoritePlaces = changeFavoritePlaces(places, place);
+    const updatedPlaces = updatePlaces(places, place);
+    const updatedNearPlaces = updateNearPlaces(places, place);
+
+    const state = {
+      favoritesPlaces: places,
+      places,
+      nearPlaces: places
+    };
+
+    expect(data(state, updateFavoritePlace(place)))
+      .toEqual({
+        places: updatedPlaces,
+        favoritesPlaces: updatedFavoritePlaces,
+        nearPlaces: updatedNearPlaces
+      });
+  });
 
   it(`Reducer should add to favorite`, () => {
     const state = {
